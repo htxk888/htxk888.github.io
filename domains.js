86f303a559b6025be60bbe17b259c4e6d1acf90a
htxk888 <xk888588@gmail.com>
2024-06-29 17:48:44 +0800

    Update domains.js

--- domains.js
+++ domains.js
@@ -1,16 +1,25 @@
 document.write('<script type="text/javascript" src="https://js.users.51.la/21865083.js"></script>');
 const domains = [
-    'http://14phh9.vip/',
-    'http://xkdy1.pro/',
-    'http://xkdy2.pro/',
-    'http://xkdy3.pro/',
-    'http://xkdy4.pro/',
-    'http://xkdy5.pro/',
-    'http://xkdy6.pro/',
-    'http://xkdy7.pro/',
-    'http://xkdy8.pro/',
-    'http://xkdy9.pro/',
-    'http://xkdy10.pro/'
+  'http://xxoo123.me',
+  'http://xxoo124.me',
+  'http://xxoo125.me',
+  'http://xxoo126.me',
+  'http://xxoo127.me',
+  'http://xxoo128.me',
+  'http://xxoo129.me',
+  'http://xxoo130.me',
+  'http://xxoo131.me',
+  'http://xxoo132.me',
+  'http://xxoo133.me',
+  'http://xxoo134.me',
+  'http://xxoo135.me',
+  'http://xxoo136.me',
+  'http://xxoo137.me',
+  'http://xxoo138.me',
+  'http://xxoo139.me',
+  'http://xxoo140.me',
+  'http://xxoo141.me',
+  'http://xxoo142.me'
 ];
 
 // 生成一个0到domains.length-1之间的随机索引
